ad82e721dcf051760657a41660250da33bd9cf8e
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-05-22 13:41:44 +0200

    Undo fasit

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,13 +22,6 @@ resource "azurerm_container_app" "devops" {
   template {
     container {
       image  = "ghcr.io/${var.repository}/${var.my_name}:latest"
-      # Task T.2:
-      #
-      # Answer T.2:
-      name   = "devops-workshop"
-      cpu    = "0.25"
-      memory = "0.5Gi"
-      #
     }
 
     min_replicas    = 1
