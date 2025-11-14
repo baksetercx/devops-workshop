a643e97ed9973082b4d30d42ad00eebde2d2f2b0
Helena <helena.dypvik.skants@computas.com>
2025-11-14 12:20:17 +0100

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,13 +15,18 @@ resource "azurerm_container_app_environment" "devops" {
 
 resource "azurerm_container_app" "devops" {
   name                         = "${var.my_name}-app"
-  container_app_environment_id = azurerm_container_app_environment.devops.id
-  resource_group_name          = azurerm_resource_group.devops.name
+  container_app_environment_id = azurerm_container_app_environment.backend_env.id
+  resource_group_name          = azurerm_resource_group.devops
   revision_mode                = "Single"
 
   template {
     container {
-      image  = "ghcr.io/${var.repository}/${var.my_name}:latest"
+      image  = "ghcr.io/computas/devops-workshop/${var.my_name}:latest"
+      # legger til disse feltene:
+      name   = "devops-workshop"
+      cpu    = "0.25"
+      memory = "0.5Gi"
+      #
     }
 
     min_replicas    = 1
